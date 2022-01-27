6d85ba225ed07ec3612f17c93645d808f8057eb9
MiksZelt <zeltins.miks24@gmail.com>
2022-01-27 14:56:07 +0200

    tabula

--- script.js
+++ script.js
@@ -1,7 +1,20 @@
-const cilv = ["Aivars Bērziņš", "Gunārs janovskis", "Jānis Krūmiņš", "Kārlis Maska", "Ivans Timoshenko"];
+const cilv = ["Aldon Hilter", "Josif Dzugashvilli", "Krišjānis Arturs Kariņš ", "Igors", "Ivans Timoshenko"];
 const balva = ["Dators ar Ryzen 9", "SAMSUNG A70", "McLaren P1", "Mercedes-Benz AMG GT Black Series", "GOOGLE"];
-const naudaKopa=100000;//kopeja summa
-let uzvaretajuSkaits=5;//uzvarētāju skaits
-let rand= Math.random()*5;
-rand=Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
-console.log(cilv[rand]);//izvada konsolē
+const naudaKopa = 100000;//kopeja summa
+let uzvaretajuSkaits = 5;//uzvarētāju skaits
+let rand = Math.random() * 5;
+let rindas = document.querySelector('.rindas');
+rindas.innerHTML = ' ';
+
+for (let i = 0; i < uzvaretajuSkaits; i++) {
+    //let rand = Math.random()*vardi.length;
+    let rand = Math.random() * 5;
+    rand = Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
+
+    let uzvaretajs = vardi[rand];
+    rindas.innerHTML += `
+<tr>
+<td>${i+1}</td>
+<td>${uzvaretajs}</td>
+</tr>`
+}
