5acb6b97994ed11a25066fd3d33e1d8dd245d8e1
MiksZelt <zeltins.miks24@gmail.com>
2022-01-27 15:02:38 +0200

    tabula.3

--- script.js
+++ script.js
@@ -2,7 +2,6 @@ const cilv = ["Aldon Hilter", "Josif Dzugashvilli", "Krišjānis Arturs Kariņš
 const balva = ["Dators ar Ryzen 9", "SAMSUNG A70", "McLaren P1", "Mercedes-Benz AMG GT Black Series", "GOOGLE"];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;//uzvarētāju skaits
-let rand = Math.random() * 5;
 let rindas = document.querySelector('.rindas');
 rindas.innerHTML = ' ';
 
