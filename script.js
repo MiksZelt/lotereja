65ee45b2d9c096edc9dd7597aee87bc378a7140f
MiksZelt <zeltins.miks24@gmail.com>
2022-02-03 15:15:06 +0200

    Loterija

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const cilv = ["Aldon Hilter", "Josif Dzugashvilli", "Krišjānis Arturs Kariņš ", "Igors", "Ivans Timoshenko"];
+const cilv = ["Oto Bismarks", "Josifs Gubers", "Artūrs Grāvītis ", "Igors", "Ivans Timoshenko"];
 const balva = ["Dators ar Ryzen 9", "SAMSUNG A70", "McLaren P1", "Mercedes-Benz AMG GT Black Series", "GOOGLE"];
 const naudaKopa = 100000;//kopeja summa
 let uzvaretajuSkaits = 5;//uzvarētāju skaits
@@ -10,10 +10,13 @@ for (let i = 0; i < uzvaretajuSkaits; i++) {
     let rand = Math.random() * 5;
     rand = Math.floor(rand);//noapaļo uz leju, math.floor - noapaļo uz leju
 
+
+    let bonus = balva[rand];
     let uzvaretajs = cilv[rand];
     rindas.innerHTML += `
 <tr>
 <td>${i + 1}</td>
 <td>${uzvaretajs}</td>
+<td>${bonus}</td>
 </tr>`
 }
